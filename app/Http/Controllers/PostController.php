48aa217dad7a259f28f7a870fedf9d49a8a343aa
minowa586 <keiminow@gmail.com>
2025-04-03 13:58:57 +0000

    editor

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,6 +12,19 @@ class PostController extends Controller
         return view('posts.index')->with(['posts' => $post->getByLimit()]);
     }
 
+    public function edit(Post $post)
+{
+    return view('posts.edit')->with(['post' => $post]);
+}
+
+public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
+}
+
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
